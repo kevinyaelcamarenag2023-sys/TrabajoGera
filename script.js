e6a7521e5bf2a07279940b2eef88257d69126f8a
kevinyaelcamarenag2023-sys <kevin.yael.camarena.g.2023@cetis162.edu.mx>
2025-10-06 09:07:53 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const slides = document.querySelector('.slides');
+const slideCount = document.querySelectorAll('.slide').length;
+let index = 0;
+
+document.querySelector('.next').addEventListener('click', () => {
+  index = (index + 1) % slideCount;
+  updateCarousel();
+});
+
+document.querySelector('.prev').addEventListener('click', () => {
+  index = (index - 1 + slideCount) % slideCount;
+  updateCarousel();
+});
+
+function updateCarousel() {
+  slides.style.transform = `translateX(-${index * 600}px)`;
+}
+
+function comprar(modelo) {
+  alert(`Has comprado el ${modelo}. ¡Gracias por tu compra!`);
+}
